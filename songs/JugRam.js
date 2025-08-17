e2797f1eae81218c4c3e8efe07f99e040e846050
bkbro404 <chaudharybibek806@gmail.com>
2025-08-17 18:41:07 +0545

    Add files via upload

---
+++ songs/JugRam.js
@@ -0,0 +1,31 @@
+window.JugRam = [
+    {
+        id: 1,
+        title: "Santi Dehuiya Hamar Parmeshor",
+        singer: "JugRam Chaudhary",
+        lyrics: `१.शान्ति देहुइया (हमार परमेश्वर) -२
+पवित्र शक्ति हमार परमेश्वर -२
+        
+२. येशू आपन (प्राण देलै) - २
+येशू हमार लाग निर्दोष बन्लै - २
+        
+३. येशू हमहन (क्षमा देलै) -२
+येशू हमार लाग रगत बगैलै -२
+
+४. येशू सबजहन (प्रेम कथै) -२
+येशू हमन मुक्ति देथै -२`,
+        chords: ``,
+        hasChords: "",
+        youtubeLink: ""
+    },
+    // {
+    //     id: ,
+    //     title: "",
+    //     singer: "JugRam Chaudhary",
+    //     lyrics: ``,
+    //     chords: ``,
+    //     hasChords: "",
+    //     youtubeLink: ""
+    // },
+    
+];
